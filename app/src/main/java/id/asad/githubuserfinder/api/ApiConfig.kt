5427258a3ad42e88045a4717cf7fe0ee980f913f
Asadullah Al Hamasy <asadhamasy@gmail.com>
2020-09-18 09:58:58 +0700

    add config http client and json pojo

--- app/src/main/java/id/asad/githubuserfinder/api/ApiConfig.kt
+++ app/src/main/java/id/asad/githubuserfinder/api/ApiConfig.kt
@@ -1,4 +1,46 @@
 package id.asad.githubuserfinder.api
 
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import id.asad.githubuserfinder.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
 object ApiConfig {
+
+    private fun  getInterceptor() : OkHttpClient {
+
+        val logging = HttpLoggingInterceptor()
+        logging.apply {
+            logging.level = HttpLoggingInterceptor.Level.BODY
+        }
+
+        return OkHttpClient.Builder()
+            .retryOnConnectionFailure(true)
+            .connectTimeout(10, TimeUnit.SECONDS)
+            .writeTimeout(10, TimeUnit.SECONDS)
+            .readTimeout(10, TimeUnit.SECONDS)
+            .addInterceptor(logging)
+            .build()
+    }
+
+    private fun initGson() : Gson {
+        return GsonBuilder().setLenient().create()
+    }
+
+    private fun getRetrofit() : Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.URL_GITHUB)
+            .addConverterFactory(GsonConverterFactory.create(initGson()))
+            .client(getInterceptor())
+            .build()
+    }
+
+    fun getInstance() : ApiService{
+        return getRetrofit().create(ApiService::class.java)
+    }
+
 }
--- app/src/main/java/id/asad/githubuserfinder/api/ApiService.kt
+++ app/src/main/java/id/asad/githubuserfinder/api/ApiService.kt
@@ -1,6 +1,16 @@
 package id.asad.githubuserfinder.api
 
+import id.asad.githubuserfinder.model.ResponseUsers
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
 interface ApiService {
+
+    @GET("search/users")
+    fun findUserByUsername(@Query("q") username : String,
+                            @Header("Authorization") token : String) : Call<ResponseUsers>
 
 
 }
