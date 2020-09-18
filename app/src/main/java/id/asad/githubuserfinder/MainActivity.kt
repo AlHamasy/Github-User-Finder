553dfdb52486a787ddfcb390bbd6d4df8b10c25b
Asadullah Al Hamasy <asadhamasy@gmail.com>
2020-09-18 10:46:27 +0700

    testing api

--- app/src/main/java/id/asad/githubuserfinder/MainActivity.kt
+++ app/src/main/java/id/asad/githubuserfinder/MainActivity.kt
@@ -2,10 +2,33 @@ package id.asad.githubuserfinder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import id.asad.githubuserfinder.api.ApiConfig
+import id.asad.githubuserfinder.model.ResponseUsers
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ApiConfig.getInstance().findUserByUsername("asad").enqueue(object : Callback<ResponseUsers> {
+            override fun onFailure(call: Call<ResponseUsers>, t: Throwable) {
+                Log.d("MainActivity", t.localizedMessage)
+            }
+            override fun onResponse(call: Call<ResponseUsers>, response: Response<ResponseUsers>) {
+                if (response.isSuccessful){
+                    val responseUsers = response.body()
+                    val items = responseUsers?.items
+
+                    items?.forEach {
+                        Log.d("MainActivity", it?.login ?: "")
+                    }
+                }
+            }
+        })
     }
 }
--- app/src/main/java/id/asad/githubuserfinder/api/ApiService.kt
+++ app/src/main/java/id/asad/githubuserfinder/api/ApiService.kt
@@ -1,16 +1,16 @@
 package id.asad.githubuserfinder.api
 
+import id.asad.githubuserfinder.BuildConfig
 import id.asad.githubuserfinder.model.ResponseUsers
 import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Header
+import retrofit2.http.Headers
 import retrofit2.http.Query
 
 interface ApiService {
 
     @GET("search/users")
-    fun findUserByUsername(@Query("q") username : String,
-                            @Header("Authorization") token : String) : Call<ResponseUsers>
-
+    @Headers("Authorization: token ${BuildConfig.TOKEN_GITHUB}")
+    fun findUserByUsername(@Query("q") username : String) : Call<ResponseUsers>
 
 }
